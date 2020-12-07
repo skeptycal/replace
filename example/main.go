e7f88651d5e633a79f1835385d5740ac46bd185c
skeptycal <skeptycal@gmail.com>
2020-12-06 23:22:20 -0600

    tidy and formatting documentation

---
+++ example/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+
+	replace "../replace"
+)
+
+func main() {
+	r := replace.New()
+
+	// reuse r to speed up parsing of
+	// different templates on the fly
+	// (for concurrency you need to protect it with a mutex)
+	err := r.Parse([]byte("@@name@@ @@animal@@"))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	m := map[string]string{
+		"animal": "Duck",
+		"name":   "Donald",
+	}
+
+	var buffer bytes.Buffer
+
+	// reuse r with a parsed template to speed up replacement
+	r.Replace(&buffer, m)
+
+	// after the replacement you may use the buffer methods Bytes(), String(), Write() or WriteTo()
+	// and reuse the same buffer after calling buffer.Reset()
+	fmt.Println(buffer.String())
+}
